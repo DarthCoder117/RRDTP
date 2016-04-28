c1c0f668e06e95583cf821f57bcfbc1a6d432afb
Tanner Mickelson <tanner@icicle-games.com>
2016-04-27 19:35:16 -0700

    Reorganizing to prepare for JNI wrapper
    Reorganizing files in order to prepare for writing the JNI wrapper.

--- cpp/source/RRDTP/Platform.h
+++ cpp/source/RRDTP/Platform.h
@@ -1,10 +1,14 @@
 #ifndef Platform_H
 #define Platform_H
 
+//Platform detection
 #if defined(_WIN32)
 #define RRDTP_PLATFORM_WINDOWS
 #elif defined(unix) || defined(__unix__) || defined(__unix) || defined(__APPLE__)
 #define RRDTP_PLATFORM_UNIX
 #endif
+
+//JNI Wrapper - comment this out to disable building the JNI wrapper
+#define BUILD_JAVA_INTERFACE
 
 #endif
