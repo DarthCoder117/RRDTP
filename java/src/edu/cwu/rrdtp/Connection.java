7e21ffacc61aeda3cacb6cfe14ca2aa991ff1d51
Tanner Mickelson <tanner@icicle-games.com>
2016-05-01 10:07:26 -0700

    Added license headers
    Added license headers to prepare for releasing the library.

--- java/src/edu/cwu/rrdtp/Connection.java
+++ java/src/edu/cwu/rrdtp/Connection.java
@@ -1,46 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*==============================================================================
+The MIT License (MIT)
+
+Copyright (c) 2016 Tanner Mickelson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+==============================================================================*/
+
 package edu.cwu.rrdtp;
 
 import com.sun.jna.Pointer;
 import com.sun.jna.NativeLong;
 
 /**
- *
- * @author super
+ * Manages an RRDTP connection. Can be run in either server or client mode.
  */
 public class Connection 
 { 
     private Pointer self = null;
    
+    /*
+    Starts the connection in server mode on the default port (4309).
+    */
     public void startServer()
     {
         startServer(4309);
     }
-    
+    /*
+    Starts the connection in server mode on a custom port.
+    */
     public void startServer(int port)
     {
         self = NativeLibrary.rrdtp_OpenServerConnection(port);
     }
     
+    /*
+    Connects to a server running RRDTP on the default port (4309).
+    */
     public void startClient(String ip)
     {
         startClient(ip, 4309);
     }
-    
+    /*
+    Connects to a server running RRDTP on a different port number.
+    */
     public void startClient(String ip, int port)
     {
         self = NativeLibrary.rrdtp_OpenClientConnection(ip, port);
     }
     
+    /*
+    Closes the connection. Works for both client and server.
+    */
     public void close()
     {
         NativeLibrary.rrdtp_CloseConnection(self);
     }
     
+    /*
+    Call this periodically to send and receive data.
+    */
     public void poll()
     {
         NativeLibrary.rrdtp_PollConnection(self);
