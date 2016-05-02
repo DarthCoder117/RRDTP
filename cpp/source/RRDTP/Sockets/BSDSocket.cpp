09ba8923697e293cf31a0ad32af1badf214f6e49
DRigby26 <drigby6@gmail.com>
2016-05-02 15:18:47 -0700

    BSD Socket implementation

--- cpp/source/RRDTP/Sockets/BSDSocket.cpp
+++ cpp/source/RRDTP/Sockets/BSDSocket.cpp
@@ -1,61 +1,107 @@
-/*==============================================================================
-The MIT License (MIT)
-
-Copyright (c) 2016 Tanner Mickelson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-==============================================================================*/
+#include <sys/proc_info.h>
 #include "BSDSocket.h"
 
 #ifdef RRDTP_PLATFORM_UNIX
 
 using namespace rrdtp;
 
+static int portNumber = 8080;
+static int m_SocketFD;
+
+
 BSDSocket::BSDSocket(DataRecievedCallback dataRecievedCallback, void* userPtr, ConnectionAcceptedCallback connectionAcceptedCallback)
-	:Socket(dataRecievedCallback, userPtr, connectionAcceptedCallback)
+    :Socket(dataRecievedCallback, userPtr, connectionAcceptedCallback)
+
 {
-	
+
+    IsServer();
+
 }
 		
 BSDSocket::~BSDSocket()
 {
-	
+
 }
 
-E_SOCKET_ERROR BSDSocket::connect(const char* ip, unsigned int port, E_SOCKET_PROTOCOL protocol)
+E_SOCKET_ERROR BSDSocket::Establish(E_SOCKET_PROTOCOL protocol)
 {
-	
+
+    if ((m_SocketFD = socket(AF_INET,SOCK_STREAM,protocol)) == -1)
+    {
+        close(m_SocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+
 }
 
-E_SOCKET_ERROR BSDSocket::listen(unsigned int port, E_SOCKET_PROTOCOL protocol)
+
+E_SOCKET_ERROR BSDSocket::Connect(const char *ip, unsigned int port, E_SOCKET_PROTOCOL protocol)
 {
-	
+    bzero(&remoteSocketInfo, sizeof(HostID));
+
+    gethostname(sysHost, MAXHOSTNAME);
+
+    if((hPtr = gethostbyname((const char *) remoteHost)) == NULL)
+    {
+        perror("System hostname misconfigured." );
+        exit(EXIT_FAILURE);
+    }
+
+    memcpy((char*)&remoteSocketInfo.sin_addr, hPtr->h_addr, hPtr->h_length);
+    remoteSocketInfo.sin_family = AF_INET;
+    remoteSocketInfo.sin_port = htons((u_short)portNumber);
+
+    if((connect(m_SocketFD, (struct sockaddr*)&remoteSocketInfo,sizeof(protocol)) < 0))
+    {
+        close(m_SocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+
 }
 
-void BSDSocket::close()
+E_SOCKET_ERROR BSDSocket::Listen(unsigned int port, E_SOCKET_PROTOCOL protocol)
 {
-	
-}
 
-size_t BSDSocket::send(const void* data, size_t sz)
-{
-	
+    if ( bind(m_SocketFD, (struct sockaddr *) &socketInfo, sizeof(struct sockaddr_in) < 0))
+    {
+        close(m_SocketFD);
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(portNumber);
+
+    int socketConnection;
+
+    if ((socketConnection = accept(m_SocketFD, NULL, NULL)) < 0)
+    {
+        close(m_SocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+
+   HostID BSDSocket::Accept(E_SOCKET_ERROR *errorCodeOut)
+    {
+
+
+
+    }
+
+    /**HostID BSDSocket::Send(const void *data, size_t sz, HostID hostID)
+    {
+
+
+    }*/
+
+    void BSDSocket :: Close(int socket){
+
+        Close(m_SocketFD);
+    }
+
 }
 
 #endif
--- cpp/source/RRDTP/Sockets/BSDSocket.h
+++ cpp/source/RRDTP/Sockets/BSDSocket.h
@@ -1,32 +1,32 @@
-/*==============================================================================
-The MIT License (MIT)
-
-Copyright (c) 2016 Tanner Mickelson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-==============================================================================*/
 #ifndef BSDSocket_H
 #define BSDSocket_H
+#define MAXHOSTNAME 255
 #include "RRDTP/Platform.h"
 #include "RRDTP/Sockets/Socket.h"
 
-#ifdef RRDTP_PLATFORM_UNIX
+#if defined(RRDTP_PLATFORM_UNIX)
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/un.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <list>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+
+
+
 
 namespace rrdtp
 {
@@ -38,19 +38,49 @@ namespace rrdtp
 		BSDSocket(DataRecievedCallback dataRecievedCallback=NULL, void* userPtr=NULL, ConnectionAcceptedCallback connectionAcceptedCallback=NULL);
 		
 		~BSDSocket();
+
+        E_SOCKET_ERROR Establish(E_SOCKET_PROTOCOL protocol);
+
+		E_SOCKET_ERROR Connect(const char *ip, unsigned int port, E_SOCKET_PROTOCOL protocol);
 		
-		E_SOCKET_ERROR connect(const char* ip, unsigned int port, E_SOCKET_PROTOCOL protocol);
-		
-		E_SOCKET_ERROR listen(unsigned int port, E_SOCKET_PROTOCOL protocol);
-		
-		void close() = 0;
-		
-		size_t send(const void* data, size_t sz);
-		
-	private:
-		
-		
-	};
+		E_SOCKET_ERROR Listen(unsigned int port, E_SOCKET_PROTOCOL protocol);
+
+		bool IsServer();
+
+		HostID GetHostID() {return m_socket;}
+
+        HostID Accept(E_SOCKET_ERROR* errorCodeOut=NULL);
+
+        static void Close(int i);
+
+		//int Send(int s, void *buffer, UINT32_MAX numBytes);
+
+		void Poll();
+
+
+    private:
+
+		char sysHost [MAXHOSTNAME+1];
+		struct sockaddr_in server;
+        struct hostent *hPtr;
+        struct sockaddr_in socketAddress;
+        struct sockaddr_in remoteSocketInfo;
+        struct sockaddr_in socketInfo;
+        int ip, m_socket;
+        struct hostNamePtr *hostNamePtr1;
+        HostID remoteHost;
+
+
+
+
+        //std::list<int> m_connectedClients;
+
+        //E_SOCKET_ERROR establish(E_SOCKET_PROTOCOL protocol);
+
+
+
+
+    };
 }
 
 #endif
