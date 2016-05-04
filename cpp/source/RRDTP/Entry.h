4e2bc7538ce01d9e7b4cac00908de4eaefbfe5ed
Tanner Mickelson <tanner@icicle-games.com>
2016-05-03 18:09:11 -0700

    Updated license headers
    Updated license headers in all C++ files.

--- cpp/source/RRDTP/Entry.h
+++ cpp/source/RRDTP/Entry.h
@@ -1,7 +1,7 @@
 /*==============================================================================
 The MIT License (MIT)
 
-Copyright (c) 2016 Tanner Mickelson
+Copyright (c) 2016 Tanner Mickelson & The RRDTP Team
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
