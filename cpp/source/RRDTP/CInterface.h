e89c2fd127bf4f4d81563c4071cada8e45fccb4b
Tanner Mickelson <tanner@icicle-games.com>
2016-05-15 15:41:19 -0700

    Export C functions
    Fixed problem where C API wasn't exporting to DLL.

--- cpp/source/RRDTP/CInterface.h
+++ cpp/source/RRDTP/CInterface.h
@@ -29,31 +29,41 @@ SOFTWARE.
 #include "RRDTP/Entry.h"
 #endif
 
+#if defined(_MSC_VER)
+//  Microsoft 
+#define DLL_EXPORT __declspec(dllexport)
+#define DLL_IMPORT __declspec(dllimport)
+#elif defined(_GCC)
+//  GCC
+#define DLL_EXPORT __attribute__((visibility("default")))
+#define DLL_IMPORT
+#endif
+
 typedef void rrdtp_Connection;
 typedef void rrdtp_Entry;
 
 extern "C"
 {
 	//Connection
-	rrdtp_Connection* rrdtp_OpenServerConnection(unsigned int port);
-	rrdtp_Connection* rrdtp_OpenClientConnection(const char* ip, unsigned int port);
+	DLL_EXPORT rrdtp_Connection* rrdtp_OpenServerConnection(unsigned int port);
+	DLL_EXPORT rrdtp_Connection* rrdtp_OpenClientConnection(const char* ip, unsigned int port);
 
-	void rrdtp_CloseConnection(rrdtp_Connection* connection);
+	DLL_EXPORT void rrdtp_CloseConnection(rrdtp_Connection* connection);
 
-	void rrdtp_PollConnection(rrdtp_Connection* connection);
+	DLL_EXPORT void rrdtp_PollConnection(rrdtp_Connection* connection);
 
 	//Entries
-	void rrdtp_SetInt(rrdtp_Connection* connection, const char* identifier, int val);
-	int rrdtp_GetInt(rrdtp_Connection* connection, const char* identifier, int defaultVal);
+	DLL_EXPORT void rrdtp_SetInt(rrdtp_Connection* connection, const char* identifier, int val);
+	DLL_EXPORT int rrdtp_GetInt(rrdtp_Connection* connection, const char* identifier, int defaultVal);
 
-	void rrdtp_SetLong(rrdtp_Connection* connection, const char* identifier, int64_t val);
-	int64_t rrdtp_GetLong(rrdtp_Connection* connection, const char* identifier, int64_t defaultVal);
+	DLL_EXPORT void rrdtp_SetLong(rrdtp_Connection* connection, const char* identifier, int64_t val);
+	DLL_EXPORT int64_t rrdtp_GetLong(rrdtp_Connection* connection, const char* identifier, int64_t defaultVal);
 
-	void rrdtp_SetBool(rrdtp_Connection* connection, const char* identifier, bool val);
-	bool rrdtp_GetBool(rrdtp_Connection* connection, const char* identifier, bool defaultVal);
+	DLL_EXPORT void rrdtp_SetBool(rrdtp_Connection* connection, const char* identifier, bool val);
+	DLL_EXPORT bool rrdtp_GetBool(rrdtp_Connection* connection, const char* identifier, bool defaultVal);
 
-	void rrdtp_SetString(rrdtp_Connection* connection, const char* identifier, const char* str);
-	const char* rrdtp_GetString(rrdtp_Connection* connection, const char* identifier, const char* defaultVal);
+	DLL_EXPORT void rrdtp_SetString(rrdtp_Connection* connection, const char* identifier, const char* str);
+	DLL_EXPORT const char* rrdtp_GetString(rrdtp_Connection* connection, const char* identifier, const char* defaultVal);
 }
 
 #endif
