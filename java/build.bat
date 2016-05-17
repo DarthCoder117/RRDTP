1fac334e75c75a7b5fbccc62f1090d81eac86137
Tanner Mickelson <tanner@icicle-games.com>
2016-05-17 14:48:44 -0700

    Fixes to Java build system
    More Java build script improvements.

--- java/build.bat
+++ java/build.bat
@@ -1,12 +1,15 @@
-../cpp/build.bat
+cd ..\cpp
+call build.bat
+cd ..\java
 rmdir /s /q build
 mkdir build
 mkdir build\win32-x86
-copy "..\cpp\lib\Release\MSVC\Release\x86\rrdtp_module.dll" "build\win32-x86\rrdtp_module.dll"
-copy "..\cpp\lib\Release\MSVC\Release\x64\rrdtp_module.dll" "build\win32-x86_64\rrdtp_module.dll"
+mkdir build\win32-x86_64
+copy "..\cpp\lib\MSVC\x86\Release\rrdtp_module.dll" "build\win32-x86\rrdtp_module.dll"
+copy "..\cpp\lib\MSVC\x64\Release\rrdtp_module.dll" "build\win32-x86_64\rrdtp_module.dll"
 javac -cp "3rd-party\*;." -d "build" src\edu\cwu\rrdtp\*.java
 cd build
 jar xf ../3rd-party/jna-4.2.2.jar
 rmdir /s /q META-INF
 jar cvf rrdtp.jar ./*
-cd ../
+cd ..\..\
