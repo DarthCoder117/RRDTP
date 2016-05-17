e314c69f859ccf2488b6847512956d4bd97eacac
Tanner Mickelson <tanner@icicle-games.com>
2016-05-17 08:50:45 -0700

    Updated Java wrapper build file
    Java wrapper build updated to support CMake.

--- cpp/build.bat
+++ cpp/build.bat
@@ -1,6 +1,11 @@
-REM Make sure to run this from the Visual Studio developer command line.
-mkdir projects
-cd projects
-cmake ../ -G "NMake Makefiles"
-nmake
-cd ../
+mkdir projects\x86
+cd projects\x86
+cmake ../../ -G "Visual Studio 14"
+cmake --build . --config Release
+cd ..\..\
+
+mkdir projects\x64
+cd projects\x64
+cmake ../../ -G "Visual Studio 14 2015 Win64"
+cmake --build . --config Release
+cd ..\..\
--- java/build.bat
+++ java/build.bat
@@ -1,8 +1,9 @@
-REM ../cpp/build.bat
+../cpp/build.bat
 rmdir /s /q build
 mkdir build
 mkdir build\win32-x86
-copy "..\cpp\lib\Release\rrdtp_module.dll" "build\win32-x86\rrdtp_module.dll"
+copy "..\cpp\lib\Release\MSVC\Release\x86\rrdtp_module.dll" "build\win32-x86\rrdtp_module.dll"
+copy "..\cpp\lib\Release\MSVC\Release\x64\rrdtp_module.dll" "build\win32-x86_64\rrdtp_module.dll"
 javac -cp "3rd-party\*;." -d "build" src\edu\cwu\rrdtp\*.java
 cd build
 jar xf ../3rd-party/jna-4.2.2.jar
