2dc244f1e139143735ed1994de741f29b33e16fe
Tanner Mickelson <tanner@icicle-games.com>
2016-05-17 07:50:15 -0700

    Added x64 project
    Visual Studio generator script now generates an x64 project as well.

--- cpp/generate-project-vs2015.bat
+++ cpp/generate-project-vs2015.bat
@@ -1,4 +1,9 @@
-mkdir projects
-cd projects
-cmake ../ -G "Visual Studio 14"
-cd ../
+mkdir projects\x86
+cd projects\x86
+cmake ../../ -G "Visual Studio 14"
+cd ..\..\
+
+mkdir projects\x64
+cd projects\x64
+cmake ../../ -G "Visual Studio 14 2015 Win64"
+cd ..\..\
