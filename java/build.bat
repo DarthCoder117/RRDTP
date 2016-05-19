e513cb80699f4e220cb2153f657669fa83e1fc68
Tanner Mickelson <tanner@icicle-games.com>
2016-05-19 07:39:58 -0700

    Fixed problem with x64 JVMs

--- java/build.bat
+++ java/build.bat
@@ -4,9 +4,9 @@ cd ..\java
 rmdir /s /q build
 mkdir build
 mkdir build\win32-x86
-mkdir build\win32-x86_64
+mkdir build\win32-x86-64
 copy "..\cpp\lib\MSVC\x86\Release\rrdtp_module.dll" "build\win32-x86\rrdtp_module.dll"
-copy "..\cpp\lib\MSVC\x64\Release\rrdtp_module.dll" "build\win32-x86_64\rrdtp_module.dll"
+copy "..\cpp\lib\MSVC\x64\Release\rrdtp_module.dll" "build\win32-x86-64\rrdtp_module.dll"
 javac -cp "3rd-party\*;." -d "build" src\edu\cwu\rrdtp\*.java
 cd build
 jar xf ../3rd-party/jna-4.2.2.jar
